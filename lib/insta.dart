83e50fcd50479d84ff011fa441b76f6d328fc3c3
MoGaaber <mohamedgaber523@ymail.com>
2020-03-10 09:25:11 +0200

    ggg

--- lib/insta.dart
+++ lib/insta.dart
@@ -9,6 +9,7 @@ import 'package:html/parser.dart' show parse;
 import 'package:http/http.dart' as http;
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 import 'dart:convert' show jsonDecode, utf8;
 
 import 'constants.dart';
@@ -21,66 +22,53 @@ class Insta extends StatelessWidget with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     Logic logic = Provider.of(context);
-    String name =
-        'اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله اكبرالله الله اك';
-    http.get('https://www.instagram.com/p/B9hFD6ShE9y/').then((x) {
-      String body = x.body;
-      var document = parse(body);
-      var yx = document
-          .querySelector('meta[property="og:title"]')
-          .attributes['content'];
-      ;
-      yx = yx.substring(yx.indexOf(':') + 3, yx.length - 1);
-      yx = yx.substring(0, 133);
-    });
-    var test =
-        'تعليق الدراسة وعزل مدن وإلغاء رحلات طيران.. إجراءات استثنائية في دول خليجية لتفادي انتشار كورونا (كوفيد-19) بعد تجاوز حالات الإصابة الـانتت';
-    String testt = 'my name is moha';
 
-    int length = 14;
-    if (testt.length > length) {
-      if (testt[length - 1] == null) {
-        length = testt.length;
-      }
-      testt = testt.trim();
-
-      print(testt[13]);
-      if (testt[length + 1] != ' ') {
-        testt = testt.substring(0, length);
-        testt = testt.substring(0, testt.lastIndexOf(' '));
-      } else {
-        testt = testt.substring(0, length);
-      }
-    }
-    return SafeArea(
-      child: Scaffold(
-        body: ListView(
-          children: <Widget>[
-            Center(
-                child: TextField(
-              controller: this.controller,
-            )),
-            Padding(padding: EdgeInsets.only(top: 50)),
-            SizedBox(
-              width: 300,
-              height: 50,
-              child: FlatButton(
-                onPressed: () async {
-                  logic.startDownload(
-                      await logic.getVideoDownloadLink(
-                          'https://www.instagram.com/p/B9cGPcJl-Qp/?igshid=13aqfn2on5s57'),
-                      'تعليق الدراسة وعزل مدن وإلغاء رحلات طيران.. إجراءات استثنائية في دول خليجية لتفادي انتشار كورونا (كوفيد-19) بعد تجاوز حالات الإصابة الـانتت');
-                },
-                child: Text(
-                  'تحميل',
-                  style: TextStyle(color: Colors.white),
-                ),
-                color: Colors.purple,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+    return FutureBuilder<bool>(
+        future: logic.checkPermission(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return !snapshot.data
+                ? Center(
+                    child: FlatButton(
+                      child: Text('Check permission'),
+                      onPressed: () {
+                        logic.notifyListeners();
+                      },
+                    ),
+                  )
+                : SafeArea(
+                    child: Scaffold(
+                      body: ListView(
+                        children: <Widget>[
+                          Center(
+                              child: TextField(
+                            controller: this.controller,
+                          )),
+                          Padding(padding: EdgeInsets.only(top: 50)),
+                          SizedBox(
+                            width: 300,
+                            height: 50,
+                            child: FlatButton(
+                              onPressed: () async {
+                                logic.startDownload(
+                                    await logic
+                                        .getVideoDownloadLink(controller.text),
+                                    Uuid().v4());
+                              },
+                              child: Text(
+                                'تحميل',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              color: Colors.purple,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        });
   }
 }
